9d251a6d9c34b8e9ce498d14002070304fb691c3
papycoda <opeyemi655@gmail.com>
2022-12-03 12:05:39 +0100

    Direct messages implemented

--- main/direct_messages/models.py
+++ main/direct_messages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class DirectMessages(models.Model):
+    id = models.AutoField(primary_key=True)
+    sender = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='receiver')
+    message = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return self.message
