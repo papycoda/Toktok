b32e3cccd65758d9137545677db251be16591aa8
papycoda <opeyemi655@gmail.com>
2022-12-03 01:47:14 +0100

    finising touches to models, comments and replies

--- main/toks/admin.py
+++ main/toks/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Toks
+from .models import *
 
 # Register your models here.
+class CommentsInline(admin.StackedInline):
+    model = TokComment
+    can_delete = True
+    verbose_name_plural = 'Comments'
 
 class TokAdmin(admin.ModelAdmin):
+    inlines = [CommentsInline,]
     list_display = ('id', 'created_by', 'tok', 'created_at')
     list_display_links = ('id', 'created_by')
     search_fields = ('created_by', 'tok')
--- main/toks/models.py
+++ main/toks/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 from users.models import User
+from users.models import generate_uuid as generate_uuid
 
 # Create your models here.
 
 class Toks(models.Model):
     '''a toks object is an object similar to a tweet, it would have an id field, a created by field, a likes field and a retweet field'''
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(
+        max_length=30,
+        primary_key=True,
+        default=generate_uuid,
+        editable=False
+    )
     # the user who created the toks
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     # the toks text
@@ -39,6 +45,10 @@ class TokComment(models.Model):
     tok = models.ForeignKey(Toks, on_delete=models.CASCADE)
     # the comment
     comment = models.CharField(max_length=500)
+    #likes for comments
+    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+    #retweets for comments
+    retoks = models.ManyToManyField(User, related_name='comment_retoks', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -46,3 +56,22 @@ class TokComment(models.Model):
     
     class Meta:
         ordering = ['-created_at']
+
+class TokCommentReply(models.Model):
+    '''replies to a comment, details include the user who replied, the comment replied to and the reply'''
+    # the user who replied
+    replied_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    # the comment replied to
+    comment = models.ForeignKey(TokComment, on_delete=models.CASCADE)
+    # the reply
+    reply = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.reply
+
+    class Meta:
+        ordering = ['-created_at']
+# to do --- link the toks comments to the toks model and the toks comments replies to the toks comments model  in admin.py
+# to do --- add a field to the toks model to count the number of comments and replies
+
